6206b8ca5630acb3c0ddf4f2dcc8856c105750bc
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-10-01 14:14:28 -0400

    replace index.html- foundations for apps inplemented; cleaned up pie.js, wordCloud.js; added styles for index.html and dependecies for foundation

--- client/app/d3/wordCloud.js
+++ client/app/d3/wordCloud.js
@@ -3,14 +3,14 @@ var createWordCloud = function (keywords) {
 
   // set width and height by actual size of animation div
   var width = document.getElementById('animation').offsetWidth;
-  var height = document.getElementById('animation').offsetHeight
+  var height = document.getElementById('animation').offsetHeight;
 
   // set color scale from -1 to 1 (this will scale with sentiment.score)
   var color = d3.scale.linear()
     .domain([-1,0,1])
     .range(["#BD0000","#D3F3EE","#009F00"]);
 
-  d3.select("#animation").selectAll("svg").remove()
+  d3.select("#animation").selectAll("svg").remove();
 
   // set word cloud rotaton, size, width
   d3.layout.cloud()
@@ -19,7 +19,7 @@ var createWordCloud = function (keywords) {
     .padding(10)
     .text(function(d) { return d.text; })
     .rotate(function() { return ~~(Math.random() * 2) * 90; }) //sets rotaton angle to 0 or 90
-    .fontSize(function(d) { return d.relevance * 60 ; }) // formula used to increase font size
+    .fontSize(function(d) { return d.relevance * 50 ; }) // formula used to increase font size
     .on("end", draw)
     .start();
 
@@ -28,6 +28,7 @@ var createWordCloud = function (keywords) {
     .attr("width", width)
     .attr("height", height)
     .attr("class", "wordcloud")
+    .style("font-family", "Impact")
     .append("g")
     // without the transform, words words would get cutoff to the left and top, they would
     // appear outside of the SVG area
