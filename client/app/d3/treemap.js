c4b129be875cb748c1185f51150a63a42cfb158d
Taylor Hayduk <taylorhayduk@gmail.com>
2015-10-02 15:27:43 -0400

    (doc) Add comments for treemap & pie, minor cleanup

--- client/app/d3/treemap.js
+++ client/app/d3/treemap.js
@@ -1,8 +1,10 @@
 var createTreeMap = function(keywords) {
 
 ////////////////////////////////////////////////////////////////////
-// START Parse keywords into treeMapData and map keywordToSentiment
+// Refactor keywords data for use in treemap
 ////////////////////////////////////////////////////////////////////
+
+  // Setup object to hold data in proper formatting for treemap
   var treeMapData = {
     "name": "master",
     "children": [
@@ -12,8 +14,10 @@ var createTreeMap = function(keywords) {
     ]
   };
 
+  // Map keyword 'name' to sentiment as new data structure doesn't hold sentiment
   var keywordToSentiment = {};
 
+  // Add keywords to treeMapData and map keywordToSentiment
   for (keyword in keywords) {
     var name = keywords[keyword].text;
     if (!keywords[keyword].sentiment.score) {
@@ -30,38 +34,42 @@ var createTreeMap = function(keywords) {
   };
 
 ////////////////////////////////////////////////////////////////////
-// END Parse keywords into treeMapData and map keywordToSentiment
 ////////////////////////////////////////////////////////////////////
 
+  // Remove previous D3 animation from #animation div
   d3.select("#animation").selectAll("svg").remove();
   d3.select("#animation").selectAll("div").remove();
 
+  // Set margin and dimensions according to #animation dimensions
   var margin = {top: 10, right: 10, bottom: 10, left: 10},
       width = document.getElementById('animation').offsetWidth - margin.left - margin.right,
       height = document.getElementById('animation').offsetHeight - margin.top - margin.bottom;
 
+  // Set color to match sentiment, red - silver - green
   var color = d3.scale.linear()
     .domain([-1,0,1])
     .range(["#BD0000","#D3F3EE","#009F00"]);
  
+  // Create treemap
   var treemap = d3.layout.treemap()
       .size([width, height])
       .sticky(true)
       .value(function(d) { return d.size; });
 
-  // not using SVG but keeping name for consistency    
-  var svg = d3.select("#animation").append("div")
+  // Create div for treemap and append to #animation  
+  var div = d3.select("#animation").append("div")
     .style("position", "relative")
     .style("width", (width + margin.left + margin.right) + "px")
     .style("height", (height + margin.top + margin.bottom) + "px")
     .style("left", margin.left + "px")
     .style("top", margin.top + "px");
 
-  var node = svg.datum(treeMapData).selectAll(".node")
+  var node = div.datum(treeMapData).selectAll(".node")
     .data(treemap.nodes)
   .enter().append("div")
     .attr("class", "node")
     .call(position)
+    // Set color by looking up d.name in keywordToSentiment
     .style("background", function(d) { return color(keywordToSentiment[d.name]);})
     .text(function(d) { return d.children ? null : d.name; });
 
