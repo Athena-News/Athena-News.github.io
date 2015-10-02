fd8cf206caba5401a8eac4bd6806c247f8ffda9e
Rod Machen <mail@rodmachen.com>
2015-10-02 11:40:51 -0500

    Move functionality into module

--- server/app.js
+++ server/app.js
@@ -1,102 +1,26 @@
-var Firebase = require('firebase');
-var AlchemyAPI = require('./alchemyapi');
-var keys = require('./keys');
-var request = require('request');
-// var express = require('express');
-// var cfenv = require('cfenv');
+'use strict';
+var express = require('express');
+var cfenv = require('cfenv');
+var fetch = require('./fetch-news');
 
-// var app = express();
-// app.use(express.static(__dirname + '/public'));
-// var appEnv = cfenv.getAppEnv();
-// app.listen(appEnv.port, function() {
-//   console.log("server starting on " + appEnv.url);
-// });
-
-var alchemyapi = new AlchemyAPI();
-var firebase = new Firebase(keys.firebase);
-
-firebase.remove();
-
-request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1?' + keys.nytimes, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      (function(num) {
-        alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
-          response.title = info.results[num].title;
-          firebase.child('Top News').child(num).set(response);
-        });
-      })(i);
-    }
-  }
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var appEnv = cfenv.getAppEnv();
+app.listen(appEnv.port, function() {
+  console.log('server starting on ' + appEnv.url);
 });
 
-request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/national/1?' + keys.nytimes, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      (function(num) {
-        alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
-          response.title = info.results[num].title;
-          firebase.child('US').child(num).set(response);
-        });
-      })(i);
-    }
-  }
-});
+// console.log(count);
+var count = 0;
 
-request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1?' + keys.nytimes, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      (function(num) {
-        alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
-          response.title = info.results[num].title;
-          firebase.child('World').child(num).set(response);
-        });
-      })(i);
-    }
-  }
-});
+var inc = function() {
+  count++;
+  console.log(count);
+};
 
-request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/opinion/1?' + keys.nytimes, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      (function(num) {
-        alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
-          response.title = info.results[num].title;
-          firebase.child('Opinion').child(num).set(response);
-        });
-      })(i);
-    }
-  }
-});
+setInterval(inc, 10000);
+setInterval(fetch, 10000);
+// setInterval(inc, 14400000);
+// setInterval(fetch, 14400000);
 
-request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/technology/1?' + keys.nytimes, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      (function(num) {
-        alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
-          response.title = info.results[num].title;
-          firebase.child('Tech').child(num).set(response);
-        });
-      })(i);
-    }
-  }
-});
-
-request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/science/1?' + keys.nytimes, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    for (var i = 0; i < 5; i++) {
-      (function(num) {
-        alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
-          response.title = info.results[num].title;
-          firebase.child('Science').child(num).set(response);
-        });
-      })(i);
-    }
-  }
-});
+// console.log(count);
---
+++ server/fetch-news.js
@@ -0,0 +1,99 @@
+'use strict';
+var request = require('request');
+var AlchemyAPI = require('./alchemyapi');
+var Firebase = require('firebase');
+var keys = require('./keys');
+
+var alchemyapi = new AlchemyAPI();
+var firebase = new Firebase(keys.firebase);
+// var firebase = new Firebase(keys.firebaseDev);
+
+module.exports = function() {
+  firebase.remove();
+
+  request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1?' + keys.nytimes, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        (function(num) {
+          alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
+            response.title = info.results[num].title;
+            firebase.child('Top News').child(num).set(response);
+          });
+        })(i);
+      }
+    }
+  });
+
+  request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/national/1?' + keys.nytimes, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        (function(num) {
+          alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
+            response.title = info.results[num].title;
+            firebase.child('US').child(num).set(response);
+          });
+        })(i);
+      }
+    }
+  });
+
+  request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1?' + keys.nytimes, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        (function(num) {
+          alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
+            response.title = info.results[num].title;
+            firebase.child('World').child(num).set(response);
+          });
+        })(i);
+      }
+    }
+  });
+
+  request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/opinion/1?' + keys.nytimes, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        (function(num) {
+          alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
+            response.title = info.results[num].title;
+            firebase.child('Opinion').child(num).set(response);
+          });
+        })(i);
+      }
+    }
+  });
+
+  request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/technology/1?' + keys.nytimes, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        (function(num) {
+          alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
+            response.title = info.results[num].title;
+            firebase.child('Tech').child(num).set(response);
+          });
+        })(i);
+      }
+    }
+  });
+
+  request('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/science/1?' + keys.nytimes, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      for (var i = 0; i < 5; i++) {
+        (function(num) {
+          alchemyapi.keywords('url', info.results[num].url, {sentiment: 1}, function(response) {
+            response.title = info.results[num].title;
+            firebase.child('Science').child(num).set(response);
+          });
+        })(i);
+      }
+    }
+  });
+
+};
+
